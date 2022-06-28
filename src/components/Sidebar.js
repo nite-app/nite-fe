110ee6e86e69a30d2e7d34cfa26f806a59bfed96
Luca Ferrario <luca.fer2907@gmail.com>
2022-06-28 17:02:41 +0200

    Menu unstyled

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { MdSpaceDashboard } from "react-icons/md";
-import { FaHome } from "react-icons/fa";
-import { RiMoonFill } from "react-icons/ri";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { BiSquareRounded } from "react-icons/bi";
+import { BiMoon } from "react-icons/bi";
 import { CgCheckR } from "react-icons/cg";
 import { IoMdSettings } from "react-icons/io";
 import {
@@ -9,45 +9,70 @@ import {
   TabsListUnstyled,
   TabPanelUnstyled,
   TabUnstyled,
+  MenuUnstyled,
+  MenuItemUnstyled, 
 } from "@mui/base";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function Sidebar({ settingsOpen, setSettingsOpen }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log("open")
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div
       className="hidden h-full w-full bg-backpanel box-border flex-col rounded-3xl p-5 mr-5 z-50
       xl:flex xl:w-3/12"
     >
       <div className="p-5 flex box-border items-center">
-        <div className="rounded-full bg-slate-400 p-4 mr-5">
+        <div className="rounded-full bg-slate-400 p-4 mr-5" onClick={handleMenuClick}>
           <p className="text-2xl font-semibold">AS</p>
         </div>
         <div className="flex flex-col">
           <h3 className="text-2xl font-semibold">Anna Shumate</h3>
         </div>
       </div>
+      <MenuUnstyled
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        className="z-50"
+      >
+        <MenuItemUnstyled onClick={handleClose}>Profile</MenuItemUnstyled>
+        <MenuItemUnstyled onClick={handleClose}>My account</MenuItemUnstyled>
+        <MenuItemUnstyled onClick={handleClose}>Logout</MenuItemUnstyled>
+      </MenuUnstyled>
       <div className="p-5 flex flex-col justify-center ">
         <TabsListUnstyled className="flex flex-col justify-center">
           <TabUnstyled value={0} className="tab-item group p-5">
-            <MdSpaceDashboard
-              size={32}
+            <MdOutlineSpaceDashboard
+              size={24}
               className="tab-icon group-hover:text-hovertxt"
             />
             <p className="tab-text group-hover:text-hovertxt">Dashboard</p>
           </TabUnstyled>
           <TabUnstyled value={1} className="tab-item group p-5">
-            <FaHome size={32} className="tab-icon group-hover:text-hovertxt" />
+            <BiSquareRounded size={20} className="tab-icon group-hover:text-hovertxt" />
             <p className="tab-text group-hover:text-hovertxt">Home</p>
           </TabUnstyled>
           <TabUnstyled value={2} className="tab-item group p-5">
-            <RiMoonFill
-              size={32}
+            <BiMoon
+              size={24}
               className="tab-icon group-hover:text-hovertxt"
             />
             <p className="tab-text group-hover:text-hovertxt">Sleep</p>
           </TabUnstyled>
           <TabUnstyled value={3} className="tab-item group p-5">
             <CgCheckR
-              size={32}
+              size={20}
               className="tab-icon group-hover:text-lighttxt"
             />
             <p className="tab-text group-hover:text-hovertxt">Habits</p>
