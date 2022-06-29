97d080dae8d37a04db4a1cd6c15056e16f82a8ac
Luca Ferrario <ferrariolucait@gmail.com>
2022-06-29 11:14:44 +0200

    settings base structure

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -21,12 +21,60 @@ function Settings({ open, onClose }) {
         id="settingsModal"
         className="box-border absolute top-0 left-0 w-full h-full overflow-hidden flex m-auto z-50 p-5 bg-white rounded-none lg:rounded-3xl lg:w-10/12 lg:h-3/4 xl:w-2/3"
       >
-        <div className="flex flex-col box-border">
+        <div className="flex flex-col box-border w-full">
           <div className="absolute top-10 right-10">
             <IoClose size={32} onClick={onClose} />
           </div>
           <div className="p-5 flex box-border items-center">
             <p className="font-semibold text-2xl">Settings</p>
+          </div>
+          <div className="flex flex-col p-5 xl:hidden">
+            <button className="p-4 bg-gray-100 text-left rounded-2xl mt-2 w-full box-border">
+              Personal
+            </button>
+            <button className="p-4 bg-gray-100 text-left rounded-2xl mt-2 w-full box-border">
+              Appearance
+            </button>
+            <button className="p-4 bg-gray-100 text-left rounded-2xl mt-2 w-full box-border">
+              About
+            </button>
+          </div>
+          <div className="hidden xl:flex flex-col mt-5">
+            <TabsUnstyled defaultValue={0} className="is-active">
+              <div className="flex box-border">
+                <div className="w-1/4 box-border">
+                  <TabsListUnstyled className="flex flex-col justify-center">
+                    <TabUnstyled value={0} className="tab-item group p-5">
+                      <p className="tab-text group-hover:text-hovertxt">
+                        Dashboard
+                      </p>
+                    </TabUnstyled>
+                    <TabUnstyled value={1} className="tab-item group p-5">
+                      <p className="tab-text group-hover:text-hovertxt">Home</p>
+                    </TabUnstyled>
+                    <TabUnstyled value={2} className="tab-item group p-5">
+                      <p className="tab-text group-hover:text-hovertxt">
+                        Sleep
+                      </p>
+                    </TabUnstyled>
+                  </TabsListUnstyled>
+                </div>
+                <div className="w-3/4 box-border">
+                  <TabPanelUnstyled
+                    value={0}
+                    className="w-full"
+                  ></TabPanelUnstyled>
+                  <TabPanelUnstyled
+                    value={1}
+                    className="w-full"
+                  ></TabPanelUnstyled>
+                  <TabPanelUnstyled
+                    value={2}
+                    className="w-full"
+                  ></TabPanelUnstyled>
+                </div>
+              </div>
+            </TabsUnstyled>
           </div>
         </div>
       </div>
