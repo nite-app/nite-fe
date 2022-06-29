bf17c594ce3714d6ca0e6505028be4782a91c36a
Luca Ferrario <ferrariolucait@gmail.com>
2022-06-29 17:44:52 +0200

    fix radius and sizing

--- src/components/settings/Appearance.js
+++ src/components/settings/Appearance.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 
 function Appearance() {
   const [lightSelected, setLightSelected] = useState(
-    "rounded-lg border-blue-600 border-4"
+    "rounded-lg border-blue-600 border-4 md:rounded-xl lg:rounded-2xl"
   );
   const [darkSelected, setDarkSelected] = useState(
-    "rounded-lg border-white border-4"
+    "rounded-lg border-white border-4 md:rounded-xl lg:rounded-2xl"
   );
   const [lightText, setLightText] = useState("✔️ Light");
   const [darkText, setDarkText] = useState("Dark");
@@ -16,28 +16,36 @@ function Appearance() {
       <div className="mt-6">
         <p className="font-semibold text-lg mb-4">Theme</p>
         <div className="flex">
-          <div className="mr-4">
+          <div className="mr-4 max-w-[250px]">
             <img
               src={require("../../img/appearancelight.png")}
               alt="appearancelight"
               className={lightSelected}
               onClick={() => {
-                setLightSelected("rounded-lg border-blue-600 border-4");
-                setDarkSelected("rounded-lg border-white border-4");
+                setLightSelected(
+                  "rounded-lg border-blue-600 border-4 md:rounded-xl lg:rounded-2xl"
+                );
+                setDarkSelected(
+                  "rounded-lg border-white border-4 md:rounded-xl lg:rounded-2xl"
+                );
                 setLightText("✔️ Light");
                 setDarkText("Dark");
               }}
             />
             <p className="font-medium text-sm mt-2">{lightText}</p>
           </div>
-          <div className="">
+          <div className="max-w-[250px]">
             <img
               src={require("../../img/appearancedark.png")}
               alt="appearancedark"
               className={darkSelected}
               onClick={() => {
-                setLightSelected("rounded-lg border-white border-4");
-                setDarkSelected("rounded-lg border-blue-600 border-4");
+                setLightSelected(
+                  "rounded-lg border-white border-4 md:rounded-xl lg:rounded-2xl"
+                );
+                setDarkSelected(
+                  "rounded-lg border-blue-600 border-4 md:rounded-xl lg:rounded-2xl"
+                );
                 setLightText("Light");
                 setDarkText("✔️ Dark");
               }}
