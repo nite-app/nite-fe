f596ad51a20b46b6aa180ba8c91276369e4d11cc
Luca Ferrario <ferrariolucait@gmail.com>
2022-07-02 00:02:23 +0200

    responsive

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 function HabitList({ children }) {
+  const childrenArray = React.Children.toArray(children);
+
   return (
-    <div className="box-border w-5/12 p-5 grid grid-rows-4 grid-cols-2 gap-x-5 gap-y-3 bg-white rounded-3xl">
+    <div className="box-border w-full p-5 flex flex-col grid-rows-4 grid-cols-2 gap-x-5 gap-y-3 bg-white rounded-3xl lg:w-6/12 xl:w-5/12 md:grid">
       <div className="flex box-border rounded-2xl p-5 py-2 items-center bg-gray-200 w-full h-14">
         {children}
       </div>
