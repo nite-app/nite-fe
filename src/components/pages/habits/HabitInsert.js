95c1a8cd1c64cccab33525ad5fc453ca930ff167
Luca Ferrario <ferrariolucait@gmail.com>
2022-07-15 18:31:16 +0200

    non va l'api boiade

---
+++ src/components/pages/habits/HabitInsert.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { FiPlus } from "react-icons/fi";
+import Axios from "axios";
+
+function HabitInsert() {
+  const [habit, setHabit] = useState("");
+
+  const addHabit = () => {
+    Axios.post("http://localhost:3001/api/addhabit", { habit: habit }).then(
+      () => {
+        alert("added habit to db");
+      }
+    );
+  };
+
+  return (
+    <div className="w-full flex items-center relative box-border rounded-2xl p-5 py-2 bg-gray-400 h-14 group">
+      <input
+        type="text"
+        placeholder="Write a new task..."
+        className="peer bg-gray-400 w-full font-medium outline-none focus:outline-none placeholder:text-gray-700"
+        onChange={(e) => {
+          setHabit(e.target.value);
+        }}
+      ></input>
+      <div className="absolute hidden right-2 top-0 h-full items-center peer-focus:flex group-hover:flex">
+        <button
+          type="submit"
+          className="flex bg-gray-100 p-2 rounded-xl w-10 h-10 justify-center items-center"
+          onClick={addHabit}
+        >
+          <FiPlus />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default HabitInsert;
--- src/components/pages/habits/LgHabitList.js
+++ src/components/pages/habits/LgHabitList.js
@@ -1,26 +1,12 @@
 import React from "react";
-import { FiPlus } from "react-icons/fi";
+import HabitInsert from "./HabitInsert";
 
 function LgHabitList({ children }) {
   const childrenArray = React.Children.toArray(children);
 
   return (
     <div className="p-5 bg-white rounded-3xl grid grid-rows-auto grid-cols-1 gap-y-3">
-      <div className="w-full flex items-center relative box-border rounded-2xl p-5 py-2 bg-gray-400 h-14 group">
-        <input
-          type="text"
-          placeholder="Write a new task..."
-          className="peer bg-gray-400 w-full font-medium outline-none focus:outline-none placeholder:text-gray-700"
-        ></input>
-        <div className="absolute hidden right-2 top-0 h-full items-center peer-focus:flex group-hover:flex">
-          <button
-            type="submit"
-            className="flex bg-gray-100 p-2 rounded-xl w-10 h-10 justify-center items-center"
-          >
-            <FiPlus />
-          </button>
-        </div>
-      </div>
+      <HabitInsert />
       {/* random icon on insert, edit with dots after add */}
       {childrenArray[0]}
       {childrenArray[1]}
