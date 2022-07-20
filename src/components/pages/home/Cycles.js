63213c1fb3ecb6ce5f88139abd3d109c73ae8804
Luca Ferrario <ferrariolucait@gmail.com>
2022-07-20 23:58:36 +0200

    styled cycles

--- src/components/pages/home/Cycles.js
+++ src/components/pages/home/Cycles.js
@@ -7,41 +7,55 @@ function Cycles() {
   const [time3, setTime3] = useState();
 
   return (
-    <div className="flex flex-col p-5 rounded-3xl bg-white">
+    <div className="flex flex-col p-10 rounded-3xl bg-white">
       <div>
-        <p>At what time to you have to wake up?</p>
-        <div>
+        <p className="font-semibold text-xl mb-5">
+          At what time do you have to wake up?
+        </p>
+        <div className="flex">
           <input
             type="number"
             name="hours"
             id=""
             placeholder="Hours"
-            min="1"
-            max="12"
             step={5}
+            className="p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-32 h-14 focus:outline-none placeholder:text-lg placeholder:text-gray-500"
           />
           <input
             type="number"
             name="minutes"
             id=""
             placeholder="Minutes"
-            min="1"
-            max="59"
             step={5}
+            className="p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-32 h-14 focus:outline-none placeholder:text-lg placeholder:text-gray-500"
           />
-          <select name="ampm" id="">
+          <select
+            name="ampm"
+            id=""
+            className="p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-28 h-14 focus:outline-none"
+          >
             <option value="am">am</option>
             <option value="pm">pm</option>
           </select>
         </div>
-        <button>Calculate</button>
+        <button className="p-3 px-5 bg-gray-800 text-white font-semibold mt-2 rounded-2xl">
+          Calculate
+        </button>
       </div>
       <div>
-        <p>Then, try to fall asleep at one of the following times:</p>
-        <div>
-          <div>{time1}</div>
-          <div>{time2}</div>
-          <div>{time3}</div>
+        <p className="font-semibold text-xl mb-5 mt-10">
+          Then, try to fall asleep at one of the following times:
+        </p>
+        <div className="flex">
+          <div className="rounded-2xl p-3 px-5 bg-gray-300 w-36 h-14 mr-2">
+            {time1}
+          </div>
+          <div className="rounded-2xl p-3 px-5 bg-gray-300 w-36 h-14 mr-2">
+            {time2}
+          </div>
+          <div className="rounded-2xl p-3 px-5 bg-gray-300 w-36 h-14 mr-2">
+            {time3}
+          </div>
         </div>
       </div>
     </div>
