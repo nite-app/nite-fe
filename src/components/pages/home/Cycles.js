92250e0aea0fbcdb97704b68a03e25b54b88f856
Luca Ferrario <ferrariolucait@gmail.com>
2022-07-21 00:13:04 +0200

    dynamic rendering

--- src/components/pages/home/Cycles.js
+++ src/components/pages/home/Cycles.js
@@ -12,14 +12,14 @@ function Cycles() {
         <p className="font-semibold text-xl mb-5">
           At what time do you have to wake up?
         </p>
-        <div className="flex">
+        <div className="flex flex-col md:flex-row">
           <input
             type="number"
             name="hours"
             id=""
             placeholder="Hours"
             step={5}
-            className="p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-32 h-14 focus:outline-none placeholder:text-lg placeholder:text-gray-500"
+            className="p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-full h-14 focus:outline-none placeholder:text-lg placeholder:text-gray-500 md:w-32"
           />
           <input
             type="number"
@@ -27,12 +27,12 @@ function Cycles() {
             id=""
             placeholder="Minutes"
             step={5}
-            className="p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-32 h-14 focus:outline-none placeholder:text-lg placeholder:text-gray-500"
+            className="mt-2 p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-full h-14 focus:outline-none placeholder:text-lg placeholder:text-gray-500 md:mt-0 md:w-32"
           />
           <select
             name="ampm"
             id=""
-            className="p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-28 h-14 focus:outline-none"
+            className="mt-2 p-3 pl-5 mr-2 bg-gray-300 rounded-2xl text-xl font-semibold w-full h-14 focus:outline-none md:mt-0 md:w-28"
           >
             <option value="am">am</option>
             <option value="pm">pm</option>
@@ -42,22 +42,47 @@ function Cycles() {
           Calculate
         </button>
       </div>
-      <div>
-        <p className="font-semibold text-xl mb-5 mt-10">
-          Then, try to fall asleep at one of the following times:
-        </p>
-        <div className="flex">
-          <div className="rounded-2xl p-3 px-5 bg-gray-300 w-36 h-14 mr-2">
-            {time1}
+      {selectedTime ? (
+        <>
+          <div>
+            <p className="font-semibold text-xl mb-5 mt-10">
+              Then, try to fall asleep at one of the following times:
+            </p>
+            <div className="flex flex-col md:flex-row">
+              <div className="mt-2 rounded-2xl p-3 px-5 bg-gray-300 w-full h-14 mr-2 md:w-36">
+                {time1}
+              </div>
+              <div className="mt-2 rounded-2xl p-3 px-5 bg-gray-300 w-full h-14 mr-2 md:w-36">
+                {time2}
+              </div>
+              <div className="mt-2 rounded-2xl p-3 px-5 bg-gray-300 w-full h-14 mr-2 md:w-36">
+                {time3}
+              </div>
+            </div>
           </div>
-          <div className="rounded-2xl p-3 px-5 bg-gray-300 w-36 h-14 mr-2">
-            {time2}
+        </>
+      ) : (
+        <>
+          <div>
+            <p className="font-semibold text-xl mb-5 mt-10">
+              If you went to bed now,
+              <br />
+              try to wake up at one of the following times:
+            </p>
+            <div className="flex flex-col md:flex-row">
+              <div className="mt-2 rounded-2xl p-3 px-5 bg-gray-300 w-full h-14 mr-2 md:w-36">
+                {time1}
+              </div>
+              <div className="mt-2 rounded-2xl p-3 px-5 bg-gray-300 w-full h-14 mr-2 md:w-36">
+                {time2}
+              </div>
+              <div className="mt-2 rounded-2xl p-3 px-5 bg-gray-300 w-full h-14 mr-2 md:w-36">
+                {time3}
+              </div>
+            </div>
           </div>
-          <div className="rounded-2xl p-3 px-5 bg-gray-300 w-36 h-14 mr-2">
-            {time3}
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
