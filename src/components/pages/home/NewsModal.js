4bcf6489af22fb525769d4078c4287cc9cea4079
Luca Ferrario <ferrariolucait@gmail.com>
2022-08-10 16:56:51 +0200

    news demo

--- src/components/pages/home/NewsModal.js
+++ src/components/pages/home/NewsModal.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IoClose } from "react-icons/io5";
 
 function NewsModal({ open, onClose }) {
-  //get from db api next
-  const [img, setImg] = useState("/img/demonw.png");
-  const [title, setTitle] = useState(
-    "Fostering Pro-Sleep Habits During the Day"
-  );
-  const [content, setContent] = useState("");
-  const [preview, setPreview] = useState(
-    "Setting the table for high-quality sleep is an all-day affair. A handful of steps that you can take during the day can pave the way for better sleep at night. "
-  );
+  let img = "/img/demonw.png";
+  const [data, setData] = useState([
+    {
+      title: "Fostering Pro-Sleep Habits During the Day",
+      img: "/img/demonw.png",
+      preview:
+        "Setting the table for high-quality sleep is an all-day affair. A handful of steps that you can take during the day can pave the way for better sleep at night.",
+      content: {
+        paragraphs: [
+          {
+            subtitle: "sub1",
+            text: "text1",
+          },
+          {
+            subtitle: "sub2",
+            text: "text2",
+          },
+        ],
+      },
+    },
+  ]);
+
+  useEffect(() => {
+    console.log(data);
+  }, []);
 
   if (!open) return null;
   return (
@@ -21,7 +37,7 @@ function NewsModal({ open, onClose }) {
       ></div>
       <div
         id="newsModal"
-        className="box-border absolute top-0 left-0 w-11/12 h-5/6 overflow-y-scroll flex m-auto z-50 p-5 bg-white rounded-3xl sm:w-10/12 sm:h-3/4 xl:w-2/3 2xl:w-3/5 max-w-[1150px] dark:bg-neutral-800 dark:shadow-md"
+        className="box-border absolute top-0 left-0 w-11/12 h-5/6 max-h-5/6 overflow-y-scroll flex m-auto z-50 p-5 bg-white rounded-3xl sm:w-10/12 sm:h-3/4 xl:w-2/3 2xl:w-3/5 max-w-[1150px] dark:bg-neutral-800 dark:shadow-md"
       >
         <div className="flex flex-col box-border w-full sm:p-5">
           <div className="relative flex z-10">
@@ -32,7 +48,7 @@ function NewsModal({ open, onClose }) {
             />
           </div>
           <div
-            className="rounded-2xl w-full mt-15 aspect-[21/9] 2xl:aspect-[21/7] bg-no-repeat bg-cover relative mb-10"
+            className="rounded-2xl w-full mt-15 aspect-[21/9] 2xl:aspect-[21/7] bg-no-repeat bg-cover relative mb-10 min-h-[200px]"
             style={{ backgroundImage: `url(${img})` }}
           >
             <img
@@ -41,10 +57,56 @@ function NewsModal({ open, onClose }) {
               className="absolute bottom-2 right-2 h-10 aspect-square sm:bottom-5 sm:right-5 sm:h-12 dark:text-neutral-100"
             />
           </div>
-          <p className="font-semibold text-2xl mb-3 dark:text-neutral-100">
-            {title}
-          </p>
-          <p className="dark:text-neutral-100">{content}</p>
+          {/* {data &&
+            data.length > 0 &&
+            data.map((item) => (
+              <p className="font-semibold text-2xl mb-3 dark:text-neutral-100">
+                {item.title}
+              </p>
+            ))}
+          {data.content.paragraphs.map((par) => {
+            return (
+              <div>
+                <p>{par.subtitle}</p>
+                <p>{par.text}</p>
+              </div>
+            );
+          })} */}
+          <div>
+            <p className="font-semibold text-2xl mb-5 dark:text-white">
+              Fostering Pro-Sleep Habits During the Day
+            </p>
+            <p className="font-medium text-lg mt-5 dark:text-neutral-200">
+              • See the Light of Day
+            </p>
+            <p className="mt-2 dark: text-neutral-300">
+              Our internal clocks are regulated by light exposure. Sunlight has
+              the strongest effect, so try to take in daylight by getting
+              outside or opening up windows or blinds to natural light. Getting
+              a dose of daylight early in the day can help normalize your
+              circadian rhythm. If natural light isn't an option, you can talk
+              with your doctor about using a light therapy box.
+            </p>
+            <p className="font-medium text-lg mt-5 dark:text-neutral-200">
+              • Find Time to Move
+            </p>
+            <p className="mt-2 mb-5 dark: text-neutral-300">
+              Daily exercise has across-the-board benefits for health, and the
+              changes it initiates in energy use and body temperature can
+              promote solid sleep. Most experts advise against intense exercise
+              close to bedtime because it may hinder your body's ability to
+              effectively settle down before sleep.
+            </p>
+            <p className="text-neutral-500 mb-10">
+              discover more{" "}
+              <a
+                href="https://www.sleepfoundation.org/sleep-hygiene/healthy-sleep-tips"
+                className="text-blue-900"
+              >
+                here
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </>
--- src/styles/vh.js
+++ src/styles/vh.js
@@ -1,5 +1,4 @@
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-const mod = document.getElementById("newsModal");
-mod.style.setProperty("--vh", `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
