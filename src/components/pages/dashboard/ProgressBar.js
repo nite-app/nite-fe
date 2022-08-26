5bb413b3d10fce6aa27acb0a590d3e9fe7d619eb
Luca Ferrario <ferrariolucait@gmail.com>
2022-08-27 00:40:56 +0200

    imp

--- src/components/pages/dashboard/ProgressBar.js
+++ src/components/pages/dashboard/ProgressBar.js
@@ -3,7 +3,7 @@ import "../../../styles/progress.css";
 
 function ProgressBar({ progress, text }) {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
+    <div className="p-2 w-full h-full flex flex-col justify-center items-center">
       <div className="rounded-full w-40 h-40" id="outer">
         <div
           className="rounded-full m-6 w-28 h-28 flex justify-center items-center"
