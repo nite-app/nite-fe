df2b7b1409ff5c3cdc4df0914e9c391a8fa03eb4
Luca Ferrario <ferrariolucait@gmail.com>
2022-08-27 00:03:37 +0200

    progress text

--- src/components/pages/dashboard/ProgressBar.js
+++ src/components/pages/dashboard/ProgressBar.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../../../styles/progress.css";
 
-function ProgressBar() {
+function ProgressBar({ progress, text }) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
       <div className="rounded-full w-40 h-40" id="outer">
-        <div className="rounded-full m-6 w-28 h-28" id="inner"></div>
+        <div
+          className="rounded-full m-6 w-28 h-28 flex justify-center items-center"
+          id="inner"
+        >
+          <p className="font-bold text-xl dark:text-white">{text}</p>
+        </div>
       </div>
     </div>
   );
