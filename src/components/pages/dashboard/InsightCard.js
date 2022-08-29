f2b34ef174b03ae1b00397ed08712a8ba7fe81cd
Luca Ferrario <ferrariolucait@gmail.com>
2022-08-29 14:49:10 +0200

    fix responsive imgs

--- src/components/pages/dashboard/InsightCard.js
+++ src/components/pages/dashboard/InsightCard.js
@@ -3,8 +3,8 @@ import React from "react";
 function InsightCard({ title, data, img }) {
   return (
     <div
-      className="min-w-max w-1/5 mr-8 rounded-xl p-5 bg-cover md:w-full"
-      style={{ backgroundImage: `url(${img})` }}
+      className="min-w-max w-1/5 mr-8 rounded-xl p-5 bg-cover bg-right bg-no-repeat md:w-full md:bg-cover"
+      style={{ backgroundImage: `url(${img})`, minWidth: 200 }}
     >
       <p className="font-semibold text-base text-gray-300 2xl:text-xl">
         {title}
