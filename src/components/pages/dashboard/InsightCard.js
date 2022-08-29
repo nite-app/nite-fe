bb6b165039f9ecc30d6e10cc02203315daf47460
Luca Ferrario <ferrariolucait@gmail.com>
2022-08-29 14:52:27 +0200

    cards hierarchy

--- src/components/pages/dashboard/InsightCard.js
+++ src/components/pages/dashboard/InsightCard.js
@@ -6,12 +6,8 @@ function InsightCard({ title, data, img }) {
       className="min-w-max w-1/5 mr-8 rounded-xl p-5 bg-cover bg-right bg-no-repeat md:w-full md:bg-cover"
       style={{ backgroundImage: `url(${img})`, minWidth: 200 }}
     >
-      <p className="font-semibold text-base text-gray-300 2xl:text-xl">
-        {title}
-      </p>
-      <p className="font-semibold text-base text-gray-400 2xl:text-xl">
-        {data}
-      </p>
+      <p className="font-semibold text-gray-400 2xl:text-lg">{title}</p>
+      <p className="font-bold text-lg text-gray-300 2xl:text-xl">{data}</p>
     </div>
   );
 }
