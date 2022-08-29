848b330480afa99062a3a48e5d5e8fbc8563836f
Luca Ferrario <ferrariolucait@gmail.com>
2022-08-29 14:41:52 +0200

    fix img

--- src/components/pages/dashboard/InsightCard.js
+++ src/components/pages/dashboard/InsightCard.js
@@ -3,7 +3,7 @@ import React from "react";
 function InsightCard({ title, data, img }) {
   return (
     <div
-      className="min-w-max w-1/5 max-w-sm h-fit mr-8 rounded-xl p-5 bg-cover md:w-full"
+      className="min-w-max w-1/5 mr-8 rounded-xl p-5 bg-cover md:w-full"
       style={{ backgroundImage: `url(${img})` }}
     >
       <p className="font-semibold text-base text-gray-300 2xl:text-xl">
