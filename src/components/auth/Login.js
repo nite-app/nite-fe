487346724b4f878493ddc348d4f6f603c03024b4
Luca Ferrario <ferrariolucait@gmail.com>
2022-09-07 23:29:05 +0200

    sizing fix

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -8,14 +8,12 @@ function Login() {
         <img
           src={require("../../img/logo.png")}
           alt="logo"
-          width={64}
-          height={64}
-          className="mr-5"
+          className="mr-4 w-14 h-14"
         />
-        <h1 className="font-bold text-3xl">nite</h1>
+        <h1 className="font-bold text-2xl">nite</h1>
       </div>
       <div className="bg-white rounded-3xl p-10 w-11/12 sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
-        <h1 className="font-semibold text-4xl">Join nite</h1>
+        <h1 className="font-semibold text-3xl">Join nite</h1>
         <p className="font-medium text-lighttxt mt-1">
           Already have an account?{" "}
           <Link to="/login" className="text-blue-700">
@@ -26,26 +24,26 @@ function Login() {
           <input
             type="text"
             placeholder="Full name"
-            className="w-full rounded-2xl bg-neutral-200 mb-3 py-5 px-8 outline-none border-none"
+            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
           />
           <input
             type="text"
             placeholder="Username"
-            className="w-full rounded-2xl bg-neutral-200 mb-3 py-5 px-8 outline-none border-none"
+            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
           />
           <input
             type="email"
             placeholder="Email"
-            className="w-full rounded-2xl bg-neutral-200 mb-3 py-5 px-8 outline-none border-none"
+            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
           />
           <input
             type="password"
             placeholder="Password"
-            className="w-full rounded-2xl bg-neutral-200 mb-3 py-5 px-8 outline-none border-none"
+            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
           />
           <button
             type="submit"
-            className="rounded-2xl mt-5 w-full bg-black p-5 text-lighttxt text-center font-semibold"
+            className="rounded-2xl mt-5 w-full bg-black p-4 text-lighttxt text-center font-semibold "
           >
             Register
           </button>
