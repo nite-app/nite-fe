4975b680883eed3ec4ec75759930003da0fa8902
Luca Ferrario <ferrariolucait@gmail.com>
2022-09-07 23:36:33 +0200

    improvements

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -12,7 +12,7 @@ function Login() {
         />
         <h1 className="font-bold text-2xl">nite</h1>
       </div>
-      <div className="bg-white rounded-3xl p-10 w-11/12 sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
+      <div className="bg-white rounded-3xl p-10 w-11/12 sm:w-4/5 md:w-3/5 lg:w-2/5 xl:w-1/3 2xl:w-1/4">
         <h1 className="font-semibold text-3xl">Join nite</h1>
         <p className="font-medium text-lighttxt mt-1">
           Already have an account?{" "}
