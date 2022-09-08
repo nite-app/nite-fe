58bc7e7ed1aa55b8ead656c3d7cedc437dc7b0cf
Luca Ferrario <ferrariolucait@gmail.com>
2022-09-08 12:12:38 +0200

    Login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -13,40 +13,33 @@ function Login() {
         <h1 className="font-bold text-2xl">nite</h1>
       </div>
       <div className="bg-white rounded-3xl p-10 w-11/12 sm:w-4/5 md:w-3/5 lg:w-2/5 xl:w-1/3 2xl:w-1/4">
-        <h1 className="font-semibold text-3xl">Join nite</h1>
+        <h1 className="font-semibold text-3xl">Log in</h1>
         <p className="font-medium text-lighttxt mt-1">
-          Already have an account?{" "}
-          <Link to="/login" className="text-blue-700">
-            Login
+          New to nite?{" "}
+          <Link to="/register" className="text-blue-700">
+            Register
           </Link>
         </p>
         <form className="mt-10">
           <input
-            type="text"
-            placeholder="Full name"
-            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
-          />
-          <input
-            type="text"
-            placeholder="Username"
-            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
-          />
-          <input
             type="email"
             placeholder="Email"
-            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
+            className="w-full rounded-2xl bg-neutral-100 mb-2 py-4 px-8 outline-none border-none "
           />
           <input
             type="password"
             placeholder="Password"
-            className="w-full rounded-2xl bg-neutral-200 mb-2 py-4 px-8 outline-none border-none "
+            className="w-full rounded-2xl bg-neutral-100 mb-2 py-4 px-8 outline-none border-none "
           />
           <button
             type="submit"
             className="rounded-2xl mt-5 w-full bg-black p-4 text-lighttxt text-center font-semibold "
           >
-            Register
+            Login
           </button>
+          <p className="font-medium text-blue-700 mt-4">
+            Forgot your password?
+          </p>
         </form>
       </div>
     </div>
