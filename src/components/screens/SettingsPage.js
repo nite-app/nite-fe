6b11c8303d7f1d30e104389379c521bab403cf4d
Luca Ferrario <ferrariolucait@gmail.com>
2022-09-10 23:03:32 +0200

    raw settings screen

--- src/components/screens/SettingsPage.js
+++ src/components/screens/SettingsPage.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../Header";
+import Personal from "../settings/Personal";
+import Appearance from "../settings/Appearance";
+import About from "../settings/About";
 
 function SettingsPage({ active, setActive }) {
   return (
     <div className="w-full h-full min-h-full box-border overflow-y-scroll rounded-3xl bg-backpanel p-8 relative 2xl:p-12 dark:bg-neutral-800">
       <Header active={active} setActive={setActive} />
+      <Personal></Personal>
+      <Appearance></Appearance>
+      <About></About>
     </div>
   );
 }
